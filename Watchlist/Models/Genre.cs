4da81753e104470f373d064b971ca06cee0dae87
VessyPenkova <99133121+VessyPenkova@users.noreply.github.com>
2022-10-21 20:41:23 +0300

    Add project files.

---
+++ Watchlist/Models/Genre.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace Watchlist.Models
+{
+    public class Genre
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(50)]
+
+        public string Name { get; set; }
+    }
+}
+
+//•	Has Id – a unique integer, Primary Key
+//•	Has Name – a string with min length 5 and max length 50 (required)
+
---
+++ Watchlist/Models/UserMovie.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.Build.Framework;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Watchlist.Models
+{
+    public class UserMovie
+    {
+        [Required]
+        public string UserId { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public User User { get; set; }
+
+        public int MovieId { get; set; }
+
+        [ForeignKey(nameof(MovieId))]
+
+        public Movie Movie { get; set; }
+
+
+    }
+}
+
+//•	UserId – a string, Primary Key, foreign key (required)
+//•	User – User
+//•	MovieId – an integer, Primary Key, foreign key (required)
+//•	Movie – Movie
+
